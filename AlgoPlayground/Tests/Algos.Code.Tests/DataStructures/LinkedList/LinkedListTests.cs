c855fd8226d9beb2613cc72d0e845b69b321679b
zenonych <i.v.savchenko@gmail.com>
2017-07-01 18:51:14 +0300

    add xunit project

---
+++ AlgoPlayground/Tests/Algos.Code.Tests/DataStructures/LinkedList/LinkedListTests.cs
@@ -0,0 +1,33 @@
+﻿using Algos.Code.DataStructures.LinkedList;
+using System;
+using Xunit;
+
+namespace Algos.Code.Tests.DataStructures.LinkedList
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            LinkedList<int> ll = new LinkedList<int>();
+            ll.Add(1);
+            ll.Add(2);
+            ll.Add(3);
+
+            ll.Delete(0);
+            ll.Delete(0);
+            ll.Delete(0);
+
+            ll.Insert(0, 10);
+            ll.Insert(0, 11);
+            ll.Insert(0, 12);
+            ll.Add(77);
+            ll.Insert(2, 555);
+
+            foreach (var item in ll)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
