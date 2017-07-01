caa5d5a52223b6f118422340fc202c7177a90a7b
zenonych <i.v.savchenko@gmail.com>
2017-07-01 19:07:04 +0300

    test green

--- AlgoPlayground/Tests/Algos.Code.Tests/DataStructures/LinkedList/LinkedListTests.cs
+++ AlgoPlayground/Tests/Algos.Code.Tests/DataStructures/LinkedList/LinkedListTests.cs
@@ -7,27 +7,44 @@ namespace Algos.Code.Tests.DataStructures.LinkedList
     public class LinkedListTests
     {
         [Fact]
-        public void Test1()
+        public void Should_Have_Exact_Inserted_Items_1()
         {
-            LinkedList<int> ll = new LinkedList<int>();
-            ll.Add(1);
-            ll.Add(2);
-            ll.Add(3);
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.Equal(list[0], 1);
+            Assert.Equal(list[1], 2);
+            Assert.Equal(list[2], 3);
+        }
 
-            ll.Delete(0);
-            ll.Delete(0);
-            ll.Delete(0);
+        [Fact]
+        public void Should_Have_Exact_Inserted_Items_2()
+        {
+            LinkedList<int> list = new LinkedList<int>();            
 
-            ll.Insert(0, 10);
-            ll.Insert(0, 11);
-            ll.Insert(0, 12);
-            ll.Add(77);
-            ll.Insert(2, 555);
+            list.Insert(0, 10);
+            list.Add(77);
+            list.Insert(1, 555);
 
-            foreach (var item in ll)
-            {
-                Console.WriteLine(item.Value);
-            }
+            Assert.Equal(list[0], 10);
+            Assert.Equal(list[1], 555);
+            Assert.Equal(list[2], 77);
+        }
+
+        [Fact]
+        public void Should_Throw_Exception()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            list.Delete(0);
+            list.Delete(0);
+            list.Delete(0);
+
+            Assert.Throws<IndexOutOfRangeException>(() => { var t = list[0]; });
         }
     }
 }
