b5e33fb7a0c0bc4ace5e707b59305ad097eac81a
zenonych <i.v.savchenko@gmail.com>
2017-07-17 18:03:31 +0300

    rook algo

--- AlgoPlayground/Algos.Code/Backtracking/RookProblem.cs
+++ AlgoPlayground/Algos.Code/Backtracking/RookProblem.cs
@@ -1,119 +1,124 @@
 ﻿using Algos.Code.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Algos.Code.Backtracking
 {
     public class RookProblem
     {
-        int rooksCount;
-        int size;
-        int[,] arr;
-        int[,] rooks;
+        private int size;
+        private int count;
+        private int[,] arr;
+
+        private int[,] rooks;
+        private Stack<int> stack = new Stack<int>();
 
         public RookProblem(int size, int count)
         {
             this.size = size;
-            rooksCount = count;
+            this.count = count;
             arr = new int[size, size];
-            rooks = new int[size + 1, 2]; //2 means index 0 for column and index 1 for row
+            rooks = new int[size + 1, 2];
+            stack.Push(1);
         }
 
         public void Solve()
         {
-            int currentRook = 1;
-            bool shouldContinue = true;
+            int solutionsFound = 0;
+            int rook = 0;
+            int startCol = 0;
+            int startRow = 0;
 
-            while (shouldContinue)
+            while (true)
             {
-                currentRook = FindPlace(currentRook, 0, 0);
+                rook = stack.Pop();
 
-                if (currentRook == rooksCount)
+                if (SetRookIfPossible(rook, startCol, startRow) && rook < count)
                 {
-                    //we found one solution, print array
-                    arr.WriteToConsole(0, size, 0, size);
+                    rook++;
+                    stack.Push(rook);
+                    startCol = 0;
+                    startRow = 0;
+                    continue;
                 }
 
-                //we've reached the end of the array                
-                var movedRook = MoveForward(currentRook);
-
-                if (movedRook == -1)
+                if (stack.Count == 0 && rook == size)
                 {
-                    break;
+                    arr.WriteToConsole(0, size, 0, size);                    
+                    solutionsFound++;
+                    Console.WriteLine(solutionsFound);
                 }
 
-                currentRook = movedRook;
+                bool moveFew;
+                arr[rooks[rook, 0], rooks[rook, 1]] = 0;
+                rooks[rook, 0] = 0;
+                rooks[rook, 1] = 0;
+
+                do
+                {
+                    moveFew = false;
+                    rook--;
+                    stack.Push(rook);
+                    int row = rooks[rook, 0];
+                    int col = rooks[rook, 1];
+
+                    rooks[rook, 0] = 0;
+                    rooks[rook, 1] = 0;
+
+                    arr[row, col] = 0;
+
+                    if (col < size - 1)
+                    {
+                        startCol = col + 1;
+                        startRow = row;
+                    }
+                    else
+                    {
+                        if (row < size - 1)
+                        {
+                            startCol = 0;
+                            startRow = row + 1;
+                        }
+                        else
+                        {
+                            moveFew = true;
+                            int onTheLastPosition = stack.Pop();
+
+                            if (onTheLastPosition == 1)
+                            {
+                                Console.WriteLine(solutionsFound);
+                                return;
+                            }
+                        }
+                    }
+                } while (moveFew);
             }
         }
 
-        private int FindPlace(int currentRook, int startCol, int startRow)
+        private bool SetRookIfPossible(int rook, int startCol, int startRow)
         {
-            int lastSet = currentRook - 1;
-            for (int i = startCol; i < size; i++)
+            for (int row = startRow; row < size; row++)
             {
-                for (int j = startRow; j < size; j++)
+                for (int col = startCol; col < size; col++)
                 {
-                    if (CanPlaceAt(i, j))
+                    if (CheckCell(row, col))
                     {
-                        lastSet = currentRook;
-
-                        arr[i, j] = currentRook;
-                        rooks[currentRook, 0] = i;
-                        rooks[currentRook, 1] = j;
-                        currentRook++;
+                        arr[row, col] = rook;
+                        rooks[rook, 0] = row;
+                        rooks[rook, 1] = col;
+                        return true;
                     }
                 }
             }
 
-            return lastSet;
+            return false;
         }
 
-        private int MoveForward(int currentRook)
-        {
-            if (currentRook == 1 && rooks[currentRook, 0] == size - 1 && rooks[currentRook, 1] == size - 1)
-            {
-                return -1;
-            }
-
-            for (int i = currentRook; i >= 1; i--)
-            {
-                var col = rooks[i, 0];
-                var row = rooks[i, 1];
-                arr[rooks[i, 0], rooks[i, 1]] = 0;
-                rooks[i, 0] = 0;
-                rooks[i, 1] = 0;
-
-                if (col < size - 1)
-                {
-                    var findPlaceResult = FindPlace(i, col + 1, row);
-                    return findPlaceResult;
-                }
-                else // equal to size - 1
-                {
-                    if (row < size - 1)
-                    {
-                        rooks[i, 0] = 0;
-                        rooks[i, 1]++;
-
-                        var findPlaceResult = FindPlace(i, col, row + 1);
-                        return findPlaceResult;
-                    }
-                    else //rook is in right bottom corner
-                    {
-                        //so do nothing, we've cleared place already
-                    }
-                }
-            }
-
-            return -1; //that means we've get to the bottom corner with first rook            
-        }
-
-        private bool CanPlaceAt(int x, int y)
+        private bool CheckCell(int row, int col)
         {
             for (int k = 0; k < size; k++)
             {
-                if (arr[k, y] != 0 || arr[x, k] != 0)
+                if (arr[row, k] != 0 || arr[k, col] != 0)
                 {
                     return false;
                 }
@@ -122,5 +127,4 @@ namespace Algos.Code.Backtracking
             return true;
         }
     }
-
 }
--- AlgoPlayground/Algos.ConsoleApp/Program.cs
+++ AlgoPlayground/Algos.ConsoleApp/Program.cs
@@ -5,7 +5,8 @@ using Algos.Code.DataStructures.LinkedList;
 using Algos.Code.Dynamic;
 using Algos.Code.Recursion;
 using System.Collections.Generic;
-using Algos.Code.Extensions;
+ using Algos.Code.Backtracking;
+ using Algos.Code.Extensions;
 
 namespace Algos.ConsoleApp
 {
@@ -13,132 +14,9 @@ namespace Algos.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var a = new int[2, 5];
-            a[1, 4] = 5;
-            a.WriteToConsole(0, 2, 0, 5);
             RookProblem problem = new RookProblem(4, 4);
             problem.Solve();
         }
-    }
-
-    public class RookProblem
-    {
-        private int size;
-        private int count;
-        private int[,] arr;
-
-        private int[,] rooks;
-        private Stack<int> stack = new Stack<int>();
-
-        public RookProblem(int size, int count)
-        {
-            this.size = size;
-            this.count = count;
-            arr = new int[size, size];
-            rooks = new int[size + 1, 2];
-            stack.Push(1);
-        }
-
-        public void Solve()
-        {
-            int solutionsFound = 0;
-            int rook = 0;
-            int startCol = 0;
-            int startRow = 0;          
-            
-            while (rook != 1 || startRow != size - 1)
-            {
-                rook = stack.Pop();
-
-                if (SetRookIfPossible(rook, startCol, startRow) && rook < count)
-                {
-                    rook++;
-                    stack.Push(rook);
-                    startCol = 0;
-                    startRow = 0;
-                    continue;
-                }
-
-                if (stack.Count == 0)
-                {
-                    arr.WriteToConsole(0, size, 0, size);
-                    solutionsFound++;
-                }
-
-                bool moveFew;                
-                arr[rooks[rook, 0], rooks[rook, 1]] = 0;
-                rooks[rook, 0] = 0;
-                rooks[rook, 1] = 0;
-
-                do
-                {
-                    moveFew = false;
-                    rook--;
-                    stack.Push(rook);
-                    int row = rooks[rook, 0];
-                    int col = rooks[rook, 1];
-
-                    rooks[rook, 0] = 0;
-                    rooks[rook, 1] = 0;
-
-                    arr[row, col] = 0;
-
-                    if (col < size - 1)
-                    {
-                        startCol = col + 1;
-                        startRow = row;
-                    }
-                    else
-                    {
-                        if (row < size - 1)
-                        {
-                            startCol = 0;
-                            startRow = row + 1;
-                        }
-                        else
-                        {
-                            moveFew = true;
-                            stack.Pop();
-                        }
-                    }
-                } while (moveFew);
-            }
-
-            Console.WriteLine(solutionsFound);
-        }
-
-        private bool SetRookIfPossible(int rook, int startCol, int startRow)
-        {
-            for (int row = startRow; row < size; row++)
-            {
-                for (int col = startCol; col < size; col++)
-                {
-                    if (CheckCell(row, col))
-                    {
-                        arr[row, col] = rook;
-                        rooks[rook, 0] = row;
-                        rooks[rook, 1] = col;
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        private bool CheckCell(int row, int col)
-        {
-            for (int k = 0; k < size; k++)
-            {
-                if(arr[row, k] != 0 || arr[k, col] != 0)                
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
     }    
 }
 
