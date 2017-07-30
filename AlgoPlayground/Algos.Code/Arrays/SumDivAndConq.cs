b709cd45b0611af376a5625c1a2b99bd9d14bf1c
zenonych <i.v.savchenko@gmail.com>
2017-07-30 20:01:28 +0300

    add sum calculationg using divide and conquer paradigm

---
+++ AlgoPlayground/Algos.Code/Arrays/SumDivAndConq.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Algos.Code.Arrays
+{
+    /// <summary>
+    /// Calculate the sum of array elements using divide and conquer paradigm 
+    /// </summary>
+    public class SumDivAndConq
+    {
+        public int Calculate(int[] arr)
+        {
+            if(arr == null || arr.Length == 0) //just a defensive part, no need for it in algo if user passes any array
+            {
+                return 0;
+            }
+
+            if (arr.Length == 1)
+            {
+                return arr[0];
+            }
+
+            int seamIndex = arr.Length / 2;
+            
+            var left = arr.Take(seamIndex).ToArray();
+            var right = arr.Skip(seamIndex).ToArray();
+
+            return Calculate(left) + Calculate(right);
+        }
+    }
+}
---
+++ AlgoPlayground/Tests/Algos.Code.Tests/Arrays/SumDivAndConqTests.cs
@@ -0,0 +1,32 @@
+﻿using Algos.Code.Arrays;
+using Xunit;
+
+namespace Algos.Code.Tests.Arrays
+{
+    public class SumDivAndConqTests
+    { 
+        [Fact]
+        public void Should_Return_33()
+        {
+            int[] input = new int[] { 2, 15, 3, 6, 7 };
+            int expected = 33;
+
+            SumDivAndConq algo = new SumDivAndConq();
+            var actual = algo.Calculate(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Return_17()
+        {
+            int expected = 17;
+            int[] input = new int[] { expected };            
+
+            SumDivAndConq algo = new SumDivAndConq();
+            var actual = algo.Calculate(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
